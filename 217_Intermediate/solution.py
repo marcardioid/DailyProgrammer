1b1f254126867ecc8c4978506c688502fc9dc6ce
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-07-02 15:26:17 +0200

    Decoding now uses an ordered dictionary for challenge 217 Intermediate

--- 217_Intermediate/solution.py
+++ 217_Intermediate/solution.py
@@ -1,20 +1,24 @@
+from collections import OrderedDict
+
 def decode(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
 
-    planets = ["Omicron V", "Hoth", "Ryza IV", "Htrae"]
-    score = lambda sentence: sum(c.isalpha() or c.isspace() for c in sentence)
+    dictionary = OrderedDict({
+        "Omicron V": (lambda sentence: (chr(int(c) ^ 0b10000) for c in sentence)),
+        "Hoth": (lambda sentence: (chr(int(c) - 10) for c in sentence)),
+        "Ryza IV": (lambda sentence: (chr(int(c) + 1) for c in sentence)),
+        "Htrae": (lambda sentence: (chr(int(c)) for c in reversed(words)))
+    })
+
     output = []
     for line in lines:
         words = line.split()
-        translations = [
-            "".join(chr(int(c) ^ 0b10000) for c in words),
-            "".join(chr(int(c) - 10) for c in words),
-            "".join(chr(int(c) + 1) for c in words),
-            "".join(chr(int(c)) for c in reversed(words))
-        ]
-        translation = max(translations, key=score)
-        planet = planets[translations.index(translation)]
+        translations = []
+        for planet, decryption in dictionary.items():
+            translations.append(''.join(decryption(words)))
+        translation = max(translations, key=(lambda sentence: sum(c.isalpha() or c.isspace() for c in sentence)))
+        planet = list(dictionary.keys())[translations.index(translation)]
         output.append(planet + " | " + translation)
 
     return '\n'.join(output)
