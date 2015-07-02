f242a77926f3e755a189e8313bd5d7fb3fbec8f6
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-07-02 16:25:07 +0200

    Added encoding and optimized decoding for challenge 217 Intermediate

--- 217_Intermediate/test_solution.py
+++ 217_Intermediate/test_solution.py
@@ -3,7 +3,7 @@ from solution import *
 
 class Test_217_Intermediate(unittest.TestCase):
     def test_decode(self):
-        self.assertEqual(decode("input1.txt"), "Htrae: We come in Peace")
+        self.assertEqual(decode("input1.txt"), "Htrae | We come in peace")
 
 if __name__ == "__main__":
     unittest.main()
