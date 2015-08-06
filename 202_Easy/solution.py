ce69ccf48069593e17092e8a2e126cbd266dac36
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-08-06 13:13:07 +0200

    Onelined solution for challenge 202 Easy

--- 202_Easy/solution.py
+++ 202_Easy/solution.py
@@ -1,11 +1,5 @@
 def binarytoascii(binary):
-    output = []
-    for i in range(0, len(binary), 8):
-        digits = binary[i:i+8]
-        num = int(digits, 2)
-        char = chr(num)
-        output.append(char)
-    return ''.join(output)
+    return ''.join([chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8)])
 
 if __name__ == "__main__":
     with open("input/input1.txt", "r") as file:
