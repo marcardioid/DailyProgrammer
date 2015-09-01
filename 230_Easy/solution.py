ef45ce086a4f4ff0bbfa3ca1b8d5b9cd99574f98
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-09-01 13:16:34 +0200

    General code cleanup and refactoring

--- 230_Easy/solution.py
+++ 230_Easy/solution.py
@@ -1,24 +1,15 @@
 import simplejson as json
 
 def find(element, search, path=[]):
-    if isinstance(element, dict):
-        for key in element:
-            if type(element[key]) in (dict, list):
-                if find(element[key], search):
-                    path.append(key)
-                    return path
-            elif element[key] == search:
-                path.append(search)
+    for key, value in element.items() if isinstance(element, dict) else enumerate(element):
+        if isinstance(value, (dict, list)):
+            if find(value, search, path):
+                path.append(key)
                 return path
-    elif isinstance(element, list):
-        for item in element:
-            if type(item) in (dict, list):
-                if find(item, search):
-                    path.append(element.index(item))
-                    return path
-            elif item == search:
-                path.append(element.index(search))
-                return path
+        elif value == search:
+            path.append(key)
+            return path
 
 if __name__ == "__main__":
-    print(" -> ".join(map(str, find(json.load(open("input/input3.txt", "r")), "dailyprogrammer")[::-1])))
+    with open("input/input3.txt", "r") as file:
+        print(" -> ".join(map(str, find(json.load(file), "dailyprogrammer")[::-1])))
