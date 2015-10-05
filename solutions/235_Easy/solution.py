590e6fab24ed48bc8329eca859430a4d305fb2d9
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-10-05 16:21:20 +0200

    Added distinct_prime_factors function to better suit the expected return type

--- solutions/235_Easy/solution.py
+++ solutions/235_Easy/solution.py
@@ -1,18 +1,18 @@
-def prime_factors(n):
+def distinct_prime_factors(n):
     i = 2
-    factors = []
+    factors = set()
     while i * i <= n:
         if n % i:
             i += 1
         else:
             n //= i
-            factors.append(i)
+            factors.add(i)
     if n > 1:
-        factors.append(n)
+        factors.add(n)
     return factors
 
 def is_ruth_aaron(pair):
-    return sum(set(prime_factors(pair[0]))) == sum(set(prime_factors(pair[1])))
+    return sum(distinct_prime_factors(pair[0])) == sum(distinct_prime_factors(pair[1]))
 
 if __name__ == "__main__":
     with open("input/input1.txt", "r") as file:
