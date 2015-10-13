ff68219f76e4461a56d2093594ae7697add60cc1
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-10-13 14:11:05 +0200

    Updated solution for challenge 235 Intermediate

--- solutions/235_Intermediate/test_solution.py
+++ solutions/235_Intermediate/test_solution.py
@@ -3,7 +3,9 @@ from solution import *
 
 class Test_235_Intermediate(unittest.TestCase):
     def test_calculate_score(self):
-        pass
+        self.assertEqual(calculate_score(['X', 'X', 'X' 'X', 'X', 'X', 'X', 'X', 'X', 'XXX']), 300)
+        self.assertEqual(calculate_score(['X', '-/', 'X' '5-', '8/', '9-', 'X', '81', '1-', '4/X']), 137)
+        self.assertEqual(calculate_score(['62', '71', 'X' '9-', '8/', 'X', 'X', '35', '72', '5/8']), 140)
 
 if __name__ == "__main__":
     unittest.main()
