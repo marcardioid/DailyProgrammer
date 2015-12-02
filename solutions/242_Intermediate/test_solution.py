a81e07ca61b76b6ba7105bc4df7ddcb7c581f7a2
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-12-02 13:02:39 +0100

    Updated solution for challenge 242 Intermediate

--- solutions/242_Intermediate/test_solution.py
+++ solutions/242_Intermediate/test_solution.py
@@ -2,7 +2,7 @@ import unittest
 from solution import *
 
 class Test_242_Intermediate(unittest.TestCase):
-    def test_weeks(self):
+    def test_program(self):
         # with open("input/input1.txt", "r") as file:
         #     fav, *timetable = file.read().splitlines()
         # self.assertEqual(program(fav, timetable), ["Pokémon", "Law & Order", "The Fresh Prince of Bel-Air"])
