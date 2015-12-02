0fae54a8b57f9e948cf466f6ecb6e5183168610f
Marc Sleegers <marcardioid@users.noreply.github.com>
2015-12-02 13:27:56 +0100

    Updated solution for challenge 243 Easy

--- solutions/243_Easy/test_solution.py
+++ solutions/243_Easy/test_solution.py
@@ -3,7 +3,10 @@ from solution import *
 
 class Test_243_Easy(unittest.TestCase):
     def test_numtype(self):
-        pass
+        self.assertEqual(numtype(18), "abundant by 3")
+        self.assertEqual(numtype(6), "perfect")
+        self.assertEqual(numtype(9), "deficient")
+        self.assertEqual(numtype(220), "abundant by 64")
 
 if __name__ == "__main__":
     unittest.main()
